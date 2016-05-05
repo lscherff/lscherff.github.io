27e724c8c76c81abb2bbb047eb85b077d3cc1cf8
lscherff <lscherff@khm.de>
2016-05-05 09:13:18 -0700

    More

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ function insideTheFence(position){
 		isInside = 1;
 		// do stuff only on change
 	}
-	document.getElementById("content").innerHTML = '37.803456 N, 122.417144 W. Negash Asegde: <a href="http://www.acheron.co/" target="_blank">Acheron</a>, Clare Bland: <a href="" target="_blank">Title</a>, Emma Carroll: <a href="" target="_blank">Title</a>, John Erbach: <a href="" target="_blank">Title</a>, Yelin Huang: <a href="" target="_blank">Title</a>, Josie Kidde: <a href="" target="_blank">Title</a>, Danny Murzhanov: <a href="" target="_blank">Title</a>, Nate Woodward: <a href="" target="_blank">Title</a>.';
+	document.getElementById("content").innerHTML = '37.803456 N, 122.417144 W. Negash Asegde: <a href="http://www.acheron.co/" target="_blank">Acheron</a>, Clare Bland: <a href="" target="_blank">Title</a>, Emma Carroll: <a href="" target="_blank">Title</a>, John Erbach: <a href="Final Icarus LLC.pdf" target="_blank">Icarus LLC</a>, Yelin Huang: <a href="" target="_blank">Title</a>, Josie Kidde: <a href="" target="_blank">Title</a>, Danny Murzhanov: <a href="" target="_blank">Title</a>, Nate Woodward: <a href="" target="_blank">Title</a>.';
 }
 
 function outsideTheFence(position) {
